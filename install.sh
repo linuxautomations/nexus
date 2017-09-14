dc6bc041ec316c35a7d54884d91f486aa192a5d3
Sriharsha Kalluru <Sriharsha.Kalluru.glu.com>
2017-09-14 15:08:53 +0530

    Updating nexus

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 ## Source Common Functions
 curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
-source /root/scripts/common-functions.sh
+#source /root/scripts/common-functions.sh
+source /tmp/common-functions.sh
 
 ## Checking Root User or not.
 CheckRoot
