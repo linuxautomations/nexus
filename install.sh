9899523c5a9e370f4783f6d01730ed74b5ed7550
Raghu K <raghuk.vit@gmail.com>
2017-09-14 06:16:36 +0000

    Updating nexus

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Source Common Functions
+curl -s "https://raw.githubusercontent.com/linuxautomations/scripts/master/common-functions.sh" >/tmp/common-functions.sh
+source /root/scripts/common-functions.sh
+
+## Checking Root User or not.
+CheckRoot
+
